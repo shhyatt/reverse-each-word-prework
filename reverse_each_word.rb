1166a58eb655315a4854e4dabdefae163808ce0a
Samantha Hyatt <shhyatt@gmail.com>
2018-12-04 17:44:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word(string)
+  new_array = []
+  array = string.split(".")
+  array.collect do |string|
+
+     new_array << string.reverse 
+    
+  end 
+  new_array.join(" ")
+
+    
+  
+  
+  
+    
+    
+  
+
+
+
+  
+  
